6386d8572184998d011933886de7ea39c887cbfe
Gustavo Henrique Alves Maciel <gustavoh.a.m1409@gmail.com>
2025-03-16 15:31:58 -0300

    att

---
+++ trabalho_aula_13.03_2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Crie um programa em C# que:
+1. Peça ao utilizador para inserir 5 nomes e os armazene numa lista.
+2. Exiba todos os nomes inseridos.
+3. Peça ao utilizador para informar um nome para pesquisar.
+4. Verifique se o nome está na lista e exiba uma mensagem informando se foi encontrado
+ou não.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+class Program 
+{
+    public static void Main() 
+    {
+        List<string> Nomes = new List<string>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Digite o {i + 1} nome: ");
+            string nome = Console.ReadLine().Trim();
+            Nomes.Add(nome);
+        }
+        Console.WriteLine("");
+        int indice = 0;
+        foreach (string nome in Nomes)
+        {
+            Console.WriteLine($"{indice + 1} Nome: {nome}");
+            indice++;
+        }
+        Console.WriteLine("");
+        Console.Write("Digite um Nome para pesquisa: ");
+        string NomePesquisa = Console.ReadLine().Trim().ToLower(); 
+
+    
+        var resultadoNome = Nomes.Where(name => name.ToLower().Contains(NomePesquisa));
+        if (resultadoNome.Any()){
+            Console.Write($"\nO nome {NomePesquisa} foi encontrado!!\n");       
+        }else{
+            Console.WriteLine("\nO nome não foi encontrado!!");
+        }
+    }
+}
---
+++ trabalho_aula_13.03_3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        List<string> tarefas = new List<string>(); 
+        bool run = true;
+        
+        while (run)
+        {
+            int resposta = 0;
+            Console.WriteLine("");
+            Console.WriteLine($"Tarefas: {tarefas.Count}");
+            Console.WriteLine("1 - Adicionar uma nova tarefa");
+            Console.WriteLine("2 - Exibir tarefas");
+            Console.WriteLine("3 - Remover uma tarefa");
+            Console.WriteLine("4 - Sair");
+            Console.Write("--> ");
+
+            bool entradaValida = int.TryParse(Console.ReadLine().Trim(), out resposta);
+
+            if (!entradaValida || !new List<int> { 1, 2, 3, 4 }.Contains(resposta))
+            {
+                Console.WriteLine("Digite um valor válido");
+            }
+            else
+            {
+                switch (resposta)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("Digite a tarefa: ");
+                        tarefas.Add(Console.ReadLine().Trim());
+                        Console.WriteLine("Tarefa adicionada.");
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        if (tarefas.Count == 0)
+                        {
+                            Console.WriteLine("Nenhuma tarefa para exibir.");
+                        }
+                        else
+                        {
+                            int contador = 1;
+                            foreach (string t in tarefas)
+                            {
+                                Console.WriteLine($"{contador}° {t}");
+                                contador++;
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        if (tarefas.Count == 0)
+                        {
+                            Console.WriteLine("Nenhuma tarefa para remover.");
+                        }
+                        else
+                        {
+                            Console.Write("Digite o índice da tarefa a ser removida: ");
+                            int indice;
+                            if (int.TryParse(Console.ReadLine(), out indice) && indice > 0 && indice <= tarefas.Count)
+                            {
+                                tarefas.RemoveAt(indice - 1); 
+                                Console.WriteLine("Tarefa removida.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Índice inválido.");
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        run = false;    
+                        Console.WriteLine("Saindo...");
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            }
+        }
+    }
+}
